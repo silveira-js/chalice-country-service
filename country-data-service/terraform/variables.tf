4e875090d7b0f7d5473825307c37821b8a0ea370
Josue Silveira <josuta@gmail.com>
2024-08-26 18:38:14 -0300

    feat: add more terraform variables

--- country-data-service/terraform/variables.tf
+++ country-data-service/terraform/variables.tf
@@ -3,6 +3,16 @@ variable "project_name" {
   default     = "country-data-service"
 }
 
+variable "github_repo" {
+  description = "The GitHub repository for the project"
+  default     = "silveira-js/test-chalice-app"
+}
+
+variable "github_branch" {
+  description = "The GitHub branch to use for deployments"
+  default     = "main"
+}
+
 variable "aws_region" {
   description = "The AWS region to deploy resources"
   default     = "us-west-2"
@@ -21,4 +31,19 @@ variable "redis_node_type" {
 variable "redis_num_cache_nodes" {
   description = "The number of cache nodes for the ElastiCache Redis cluster"
   default     = 1
+}
+
+variable "codebuild_compute_type" {
+  description = "The compute type for CodeBuild project"
+  default     = "BUILD_GENERAL1_MEDIUM"
+}
+
+variable "codebuild_image" {
+  description = "The Docker image to use for the CodeBuild project"
+  default     = "aws/codebuild/standard:6.0"
+}
+
+variable "github_connection_arn" {
+  description = "The ARN of the GitHub connection for CodePipeline"
+  type        = "string"
 }
